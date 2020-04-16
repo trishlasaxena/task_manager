f7d44430671508915aa09f1f052f0cb31d6f00f5
Trishla Saxena <trishla.saxena@nagarro.com>
2020-04-17 01:30:53 +0530

    updated server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use('/', express.static(__dirname + '/public'))
+app.use('/', express.static(__dirname + ''))
 
 app.use('/todos', todoRoute)
 
