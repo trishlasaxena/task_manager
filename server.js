d8f2fa01761c8c12d31d41d665dfc43fbcd4fbc9
Trishla Saxena <trishla.saxena@nagarro.com>
2020-04-17 12:05:22 +0530

    env port added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-
+const path = require('path')
+const SERVER_PORT = process.env.PORT || 3333
 const { db } = require('./db')
 const todoRoute = require('./route/todo')
 
@@ -8,13 +9,14 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use('/', express.static(__dirname + ''))
+//app.use('/', express.static(__dirname + ''))
+app.use('/',express.static(path.join(__dirname,'public')))
 
 app.use('/todos', todoRoute)
 
 db.sync()
   .then(() => {
-    app.listen(1500)
+    app.listen(SERVER_PORT)
   })
   .catch((err) => {
     console.error(err)
